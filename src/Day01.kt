15a5d871ab92d8e5ba36aaca1130191f643effe3
carotkut94 <carotkut12@gmail.com>
2021-12-02 14:09:20 +0530

    feat(DAY-01): solution for day one aoc problem

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,21 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increasingCount = 0
+        input.reduce { prev, curr ->  if (curr.toInt() > prev.toInt()) increasingCount++; curr }
+        return increasingCount
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val windowsSum = input.windowed(size = 3, step = 1).map{
+            it.map{it.toInt()}.toList().sum().toString()
+        }
+        return part1(windowsSum)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
