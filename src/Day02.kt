06d26c609004a8e9a93680387c22ff3eb2e4396e
carotkut94 <carotkut12@gmail.com>
2021-12-07 13:15:08 +0530

    feat(day-2-dive): part 2 solution for day 2 aoc

--- src/Day02.kt
+++ src/Day02.kt
@@ -2,19 +2,28 @@ import java.io.File
 
 fun main(){
 
-    var horizontal = 0
-    var depth = 0
-    File("src/Day02.txt")
+    val moves = File("src/Day02.txt")
         .readLines()
         .map { it.split(" ") }
         .map {
             val count = it[1].toInt()
             when(it[0]) {
-                "forward" -> horizontal+=count
-                "up" -> depth-=count
-                else -> depth+=count
+                "forward" -> Pair(+count, 0)
+                "up" -> Pair(0, -count)
+                else -> Pair(0, +count)
             }
         }
 
+    val (horizontal, depth) = moves.reduce { (h,d), (x,y) -> Pair(h+x, d+y) }
+
     println(horizontal*depth)
+
+    // Part 2, with aim inclusion
+
+    val (horizontal1, depth1, aim1) = moves.fold(
+        initial = Triple(0,0,0),
+        operation = {(h,d,a),(x,y)->Triple(h+x,d+a*x, a+y)}
+    )
+
+    println(horizontal1*depth1)
 }
